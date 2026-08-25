93de6ae268ba2ddd2ff56a5f35b0d5f451361715
Tevezx <teveztwitch@gmail.com>
2026-08-25 10:13:45 -0300

    chore: adicionando setup inicial em java e springboot com dependencias

---
+++ cineplay/src/test/java/play/cine/cineplay/CineplayApplicationTests.java
@@ -0,0 +1,13 @@
+package play.cine.cineplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
