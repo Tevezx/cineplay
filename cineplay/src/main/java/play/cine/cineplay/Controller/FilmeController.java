4422f76c137fb7bb1557c2fc365bf182d910cf02
Tevezx <teveztwitch@gmail.com>
2026-08-25 12:05:52 -0300

    feat: adicionando get filmes ao controller

--- cineplay/src/main/java/play/cine/cineplay/Controller/FilmeController.java
+++ cineplay/src/main/java/play/cine/cineplay/Controller/FilmeController.java
@@ -1,8 +1,14 @@
 package play.cine.cineplay.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import play.cine.cineplay.model.Filme;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/filmes")
@@ -11,5 +17,15 @@ public class FilmeController {
 
     public FilmeController(JdbcTemplate template) {
         this.template = template;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Filme>> findAll(){
+        String sql = "SELECT * FROM filme";
+
+        List<Filme> filmes = template.query(sql,
+                new BeanPropertyRowMapper<>(Filme.class));
+
+        return ResponseEntity.ok(filmes);
     }
 }
