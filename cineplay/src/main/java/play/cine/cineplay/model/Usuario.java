f17f983b40b1f7e1c772455fe46d9878d622b7f7
Tevezx <teveztwitch@gmail.com>
2026-08-26 09:33:42 -0300

    feat: adicionando model de usuario

---
+++ cineplay/src/main/java/play/cine/cineplay/model/Usuario.java
@@ -0,0 +1,71 @@
+package play.cine.cineplay.model;
+
+public class Usuario {
+    private Integer id;
+    private Character cpf;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, Character cpf, String nome, String email, String senha) {
+        this.id = id;
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", cpf=" + cpf +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Character getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Character cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
