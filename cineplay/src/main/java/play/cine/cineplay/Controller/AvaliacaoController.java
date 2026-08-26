359d39b5241cb52f6b8250a88bbe1fbf85c9f95e
Tevezx <teveztwitch@gmail.com>
2026-08-26 11:17:51 -0300

    feat: adicionando get para as avaliacoes e atualizando tabela do sql para adicionar 'comentarios'

--- cineplay/src/main/java/play/cine/cineplay/Controller/AvaliacaoController.java
+++ cineplay/src/main/java/play/cine/cineplay/Controller/AvaliacaoController.java
@@ -1,8 +1,14 @@
 package play.cine.cineplay.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import play.cine.cineplay.model.Avaliacao;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("v1/avaliacoes")
@@ -11,5 +17,15 @@ public class AvaliacaoController {
 
     public AvaliacaoController(JdbcTemplate template) {
         this.template = template;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Avaliacao>> findAll() {
+        String sql = "SELECT usuario_id_usuario AS id_usuario, filme_id_filme AS id_filme, nota, comentario FROM avaliacao";
+
+        List<Avaliacao> avaliacoes = template.query(sql,
+                new BeanPropertyRowMapper<>(Avaliacao.class));
+
+        return ResponseEntity.ok(avaliacoes);
     }
 }
