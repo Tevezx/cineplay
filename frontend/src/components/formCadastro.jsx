beae276b9462939d337bc5aae16c153d982e09ab
Tevezx <teveztwitch@gmail.com>
2026-09-03 00:11:43 -0300

    feat: adicionando estrutura de component ao formulario de cadastro

--- frontend/src/components/formCadastro.jsx
+++ frontend/src/components/formCadastro.jsx
@@ -1,9 +1,29 @@
 export function FormCadastro() {
-    return(
+    const [formData, setFormData] = useState({
+        cpf: '',
+        nome: '',
+        email: '',
+        senha: '',
+    });
+
+    return (
         <form>
-            <label>
-                <input></input>
-            </label>
+            <div>
+                <label>CPF:</label>
+                <input type="text" id="cpf" name="cpf" />
+            </div>
+            <div>
+                <label>Nome:</label>
+                <input type="text" id="nome" name="nome" />
+            </div>
+            <div>
+                <label>Email:</label>
+                <input type="email" id="email" name="email" />
+            </div>
+            <div>
+                <label>Senha:</label>
+                <input type="password" id="senha" name="senha" />
+            </div>
             <button>Cadastrar</button>
         </form>
     );
