a324e29838d4390c82e0543ebd8a3284d00d88d8
Tevezx <teveztwitch@gmail.com>
2026-09-02 23:59:35 -0300

    feat: adicionando base de formulario

---
+++ frontend/src/components/formCadastro.jsx
@@ -0,0 +1,10 @@
+export function FormCadastro() {
+    return(
+        <form>
+            <label>
+                <input></input>
+            </label>
+            <button>Cadastrar</button>
+        </form>
+    );
+}
